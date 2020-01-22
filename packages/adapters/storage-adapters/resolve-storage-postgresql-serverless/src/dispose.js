2fd7697943d3307fefa6481b8717a946f8ef97d5
mrcheater <mister.cheater@list.ru>
2020-01-22 12:33:03 +0300

    Revert

--- packages/adapters/storage-adapters/resolve-storage-postgresql-serverless/src/dispose.js
+++ packages/adapters/storage-adapters/resolve-storage-postgresql-serverless/src/dispose.js
@@ -1,16 +1,3 @@
-const dispose = async pool => {
-  try {
-    await pool.rdsDataService
-      .executeStatement({
-        resourceArn: pool.dbClusterOrInstanceArn,
-        secretArn: pool.awsSecretStoreArn,
-        database: 'postgres',
-        continueAfterTimeout: false,
-        includeResultMetadata: true,
-        sql: `SELECT pg_terminate_backend(pg_backend_pid());`
-      })
-      .promise()
-  } catch (error) {}
-}
+const dispose = async () => {}
 
 export default dispose
